e5769e337d1a4ccb54acd1581b095b7c3dec3f2f
Mr.Armah <43911341+Cloudy-Skies@users.noreply.github.com>
2022-10-19 19:50:36 +0000

    set up db connection and db query functionality

---
+++ Settings/db_class.php
@@ -0,0 +1,40 @@
+<?php
+//database credentials
+require('db_cred.php');
+
+class db_connection{
+    //properties
+    public $db = null;
+    public $result = null;
+
+    function db_connect(){
+        $this->db = mysqli_connect(SERVERNAME,HOSTNAME,PASSWORD,DATABASE);
+
+        //test the connection
+        if(mysqli_connect_errno()){
+            return false;
+        }else {
+            return true;
+        }
+    }
+
+    function db_query($query)
+    {
+        if (!$this->db_connect()) {
+			return false;
+		} 
+		elseif ($this->db==null) {
+			return false;
+		}
+
+        //run 
+        $this->results = mysqli_query($this->db,$query);
+		
+		if ($this->results == false) {
+			return false;
+		}else{
+			return true;
+		}
+    }
+}
+?>
