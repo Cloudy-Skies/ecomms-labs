b2ca6091d038b3b2f1eb7268b9a4b57b0f7877dc
Mr.Armah <43911341+Cloudy-Skies@users.noreply.github.com>
2022-10-19 18:53:58 +0000

    added db_cred file

---
+++ Settings/db_cred.php
@@ -0,0 +1,8 @@
+<?php
+//Database Credentials
+define('SERVERNAME','eu-cdbr-west-03.cleardb.net');
+define('HOSTNAME', 'b1ecbef9f5bea1');
+define('PASSWORD', '6609bca5');
+define('DATABASE','heroku_67ace1309e9dc31');
+
+?>
