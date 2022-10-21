4de0e260f9098c1ddbc551b86de9c332e3b85e1c
Mr.Armah <43911341+Cloudy-Skies@users.noreply.github.com>
2022-10-21 08:04:28 +0000

    reversed changes

--- Login/login.php
+++ Login/login.php
@@ -10,48 +10,7 @@
 </head>
 
 <body>
-    /*
-    @author mdbootstrap.com
-     */
-    <form action="/Login/loginprocess.php">
-        <!-- Email input -->
-        <div class="form-outline mb-4">
-            <input type="email" id="form2Example1" class="form-control" placeholder="Enter your email here..."/>
-            <label class="form-label" for="form2Example1">Email address</label>
-        </div>
-
-        <!-- Password input -->
-        <div class="form-outline mb-4">
-            <input type="hidden" id="form2Example2" class="form-control" placeholder="Enter your password here..."/>
-            <label class="form-label" for="form2Example2">Password</label>
-        </div>
-
-        <!-- 2 column grid layout for inline styling -->
-        <div class="row mb-4">
-            <div class="col d-flex justify-content-center">
-                <!-- Checkbox -->
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="form2Example31" checked />
-                    <label class="form-check-label" for="form2Example31"> Remember me </label>
-                </div>
-            </div>
-
-            <div class="col">
-                <!-- Simple link -->
-                <a href="#!">Forgot password?</a>
-            </div>
-        </div>
-
-        <!-- Submit button -->
-        <button type="button" class="btn btn-primary btn-block mb-4"><input type="submit" value="error"> Sign in</button>
-
-    </form>
-    <!-- Register buttons -->
-    <div class="text-center">
-        <p>Not a member? <a href="/Login/register.php">Register</a></p>
-        <p>or sign up with:</p>
-        </button>
-    </div>
+    <button type="submit"></button>
 </body>
 
 </html>
