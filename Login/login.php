cf689d5b60bfc7c8a5c456c92640221607f62752
Mr.Armah <43911341+Cloudy-Skies@users.noreply.github.com>
2022-10-28 15:33:44 +0000

    added login functionality

--- Login/login.php
+++ Login/login.php
@@ -10,7 +10,32 @@
 </head>
 
 <body>
-    <button type="submit"></button>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="mt-5">Create Record</h2>
+                    <p>Please fill this form and submit to add Customer record to the database.</p>
+                    <form action="loginprocess.php" method="POST">
+                        <div class="form-group">
+                            <label>Email</label>
+                            <textarea name="email" class="form-control" placeholder="Enter email..." required></textarea>
+                            <span class="invalid-feedback"></span>
+                        </div>
+                        <div class="form-group">
+                            <label>Password</label>
+                            <textarea name="password" class="form-control" placeholder="Enter password..." required></textarea>
+                            <span class="invalid-feedback"></span>
+                        </div>
+
+                        //submit buttons
+                        <input type="submit" class="btn btn-primary" value="Submit" name="addLogin">
+                        <a href="index.php" class="btn btn-secondary ml-2">Cancel</a>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
--- Login/loginprocess.php
+++ Login/loginprocess.php
@@ -1,4 +1,29 @@
 <?php
-    $pword = $_POST['customer_pass'];
-    $email = $_POST['customer_email'];
+//     $pword = $_POST['customer_pass'];
+//     $email = $_POST['customer_email'];
+// 
+include "../Controllers/customer_controller.php"
+
+if (ISSET($_POST['addSubmit'])) {
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $password=password_hash($_POST['password'],PASSWORD_BCRYPT);
+    $country=$_POST['country'];
+    $city=$_POST['city'];
+    $number=$_POST['cnumber'];
+    $email=$_POST['email'];
+    $image=$_POST['image'];
+
+    // echo $name;
+    // echo $email;
+    // echo $password;
+    // echo $country;
+    // echo $city;
+    // echo $number;
+    // echo $email;
+    //addCustomer_ctr($name,$email,$password,$country,$city,$number,$image);
+    var_dump(addCustomer_ctr($name,$email,$password,$country,$city,$number,$image));
+}
+
+
 ?>
