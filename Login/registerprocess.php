7083b7ad31fffac1c61be5c22082becedfc06020
Mr.Armah <43911341+Cloudy-Skies@users.noreply.github.com>
2022-10-28 13:05:06 +0000

    fixing registerprocess

--- Login/registerprocess.php
+++ Login/registerprocess.php
@@ -5,10 +5,15 @@ include"../Controllers/customer_controller.php";
 if (ISSET($_POST['addSubmit'])) {
     $name=$_POST['name'];
     $email=$_POST['email'];
-    $password=;
+    $password=password_hash($_POST['password'],PASSWORD_BCRYPT);
     $country=$_POST['country'];
     $city=$_POST['city'];
-    $cnumber=$_POST['cnumber'];
+    $number=$_POST['cnumber'];
     $email=$_POST['email'];
+    $image=$_POST['image'];
+
+    echo $name;
+
+    addCustomer_ctr($name,$email,$password,$country,$city,$number,$image);
 }
 ?>
