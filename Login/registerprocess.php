f9aebdb3870ef87ac33473b60bd8f9bff48751d6
Mr.Armah <43911341+Cloudy-Skies@users.noreply.github.com>
2022-10-28 13:04:09 +0000

    added register to registerprocess

---
+++ Login/registerprocess.php
@@ -0,0 +1,14 @@
+<?php
+
+include"../Controllers/customer_controller.php";
+
+if (ISSET($_POST['addSubmit'])) {
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $password=;
+    $country=$_POST['country'];
+    $city=$_POST['city'];
+    $cnumber=$_POST['cnumber'];
+    $email=$_POST['email'];
+}
+?>
