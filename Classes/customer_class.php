61dd4936d6264de2d842d5078cab2fce85dfc6b8
Mr.Armah <43911341+Cloudy-Skies@users.noreply.github.com>
2022-10-31 17:57:07 +0000

    improved customer model with more functions

--- Classes/customer_class.php
+++ Classes/customer_class.php
@@ -3,11 +3,9 @@ require ('../Settings/db_class.php');
 
 class CustomerClass extends db_connection
 {
-    
-    
     //add function
     //expects 7
-    public function addCustomer_cls($cname,$cemail,$cpass,$ccountry,$ccity,$cnum,$cimage)
+    public function add_customer_cls($name,$email,$pass,$country,$city,$contact,$image)
     {
         //new user has user role2
         $user_role='2';
@@ -16,24 +14,26 @@ class CustomerClass extends db_connection
         customer_country,customer_city,customer_contact,
         customer_image,user_role) 
         VALUES 
-        ('$cname','$cemail','$cpass','$ccountry','$ccity','$cnum','$cimage',$user_role)";
+        ('$name','$email','$pass','$country','$city','$contact','$image',$user_role)";
 
         return $this->db_query($sql);
     }
 
     //TODO
-    function editCustomer()
+    function edit_customer_cls($id,$name,$email,$country,$city,$contact)
     {
-        # code...
+        $sql= "UPDATE customer set customer_name=$name,customer_email=$email,customer_country=$country,customer_city=$city,customer_contact=$contact WHERE id = $id";
+        return $this->db_query($sql);
     }
 
     //TODO
-    function delCustomer()
+    function delete_customer_cls($id)
     {
-        # code...
+        $sql="DELETE from customer WHERE customer_id=$id";
+        return $this->db_query($sql);
     }
 
-    function selectCustomerEmail_cls($email)
+    function select_one_customer_cls($email)
     {
         $sql = "SELECT * from customer where 'customer_email'=$email";
         return $this->db_fetch_one($sql);
