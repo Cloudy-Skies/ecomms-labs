a83dbdbf812c787a4a33677087381eb9aa0b52a7
Mr.Armah <43911341+Cloudy-Skies@users.noreply.github.com>
2022-11-08 16:38:03 +0000

    page to display all products

---
+++ View/all_product.php
@@ -0,0 +1,38 @@
+<?php
+check_login();
+if (check_permission()!= 1){
+    header('Location: ../index.php');
+} 
+
+$brands = select_all_brands_controller();
+$categories = select_all_categories_ctr();
+$products = select_all_categories_ctr();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link
+            rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+            integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+            crossorigin="anonymous"
+        />
+
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+        <!-- CSS Styles -->
+        <link rel="stylesheet" href="../css/product.css">
+
+</head>
+<body>
+    
+</body>
+</html>
