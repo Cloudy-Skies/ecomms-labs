561db829428a17075f53ebbbb7fc7d2a889637cc
Mr.Armah <43911341+Cloudy-Skies@users.noreply.github.com>
2022-11-08 10:00:22 +0000

    db cred edit

--- Settings/db_cred.php
+++ Settings/db_cred.php
@@ -1,13 +1,13 @@
 <?php
-//Database Credentials
-// define('SERVERNAME','eu-cdbr-west-03.cleardb.net');
-// define('HOSTNAME', 'b1ecbef9f5bea1');
-// define('PASSWORD', '6609bca5');
-// define('DATABASE','heroku_67ace1309e9dc31');
+// Database Credentials
+define('SERVERNAME','eu-cdbr-west-03.cleardb.net');
+define('HOSTNAME', 'b1ecbef9f5bea1');
+define('PASSWORD', '6609bca5');
+define('DATABASE','heroku_67ace1309e9dc31');
 
-define('SERVERNAME','localhost');
-define('HOSTNAME', 'root');
-define('PASSWORD', '');
-define('DATABASE','shoppn');
+// define('SERVERNAME','localhost');
+// define('HOSTNAME', 'root');
+// define('PASSWORD', '');
+// define('DATABASE','shoppn');
 
 ?>
