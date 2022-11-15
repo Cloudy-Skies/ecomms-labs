238939e3f22db46cde47960987ba8448f837f46d
Mr.Armah <43911341+Cloudy-Skies@users.noreply.github.com>
2022-11-15 01:31:39 +0000

    improved customer ctr

--- Controllers/customer_controller.php
+++ Controllers/customer_controller.php
@@ -4,14 +4,15 @@ require ('../Classes/customer_class.php');
 //edit,select,update, delete function
 
 //takes 7 vars
-function add_customer_controller($name,$email,$password,$country,$city,$contact,$role){
-    $customer_instance = new CustomerClass();
-    return $customer_instance->add_customer_cls($name, $email, $password, $country, $city, $contact, $role);
+function add_customer_controller($name,$email,$password,$country,$city,$contact,$image){
+    $customer_instance = new customer_class();
+    $customer_instance->add_customer_cls($name, $email, $password, $country, $city, $contact, $image);
+    return $customer_instance;
 }
 
 
 function select_one_customer_controller($email){
-    $select_one_customer = new CustomerClass();
+    $select_one_customer = new customer_class();
 
     return  $select_one_customer->select_one_customer_cls($email);
     //return $actionItem;
@@ -19,7 +20,7 @@ function select_one_customer_controller($email){
 }
 
 function selCustomerPassword_ctr($password){
-    $select_customer_password = new CustomerClass();
+    $select_customer_password = new customer_class();
 
    return $select_customer_password->selectCustomerPassword_cls($password);
     //var_dump($selectpassword);
@@ -35,6 +36,6 @@ function selCustomerPassword_ctr($password){
 
 function edit_customer_controller($id,$name,$email,$country,$city,$contact)
 {
-    $customer_instance = new CustomerClass();
+    $customer_instance = new customer_class();
     return $customer_instance->edit_customer_cls($id,$name,$email,$country,$city,$contact);
 }
