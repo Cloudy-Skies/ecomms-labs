ac0c45023878399edf0f6be09b1c06cf79d7f044
Mr.Armah <43911341+Cloudy-Skies@users.noreply.github.com>
2022-11-15 01:14:42 +0000

    Index now with hidden links for admin and customer

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
+<?php
+require 'Settings/core.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,62 +12,90 @@
     <title>Home</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
+
 <body>
     <h1>Ecomms Labs</h1>
+    -- <?php
+        // checks user login status and user role
+        //code below runs if user is logged in
+        if (isset($_SESSION["loggedIn"]) && $_SESSION["loggedIn"] === true) {
+            //checks if user is admin
+            if ($_SESSION["user_role"] == 1) {
+                echo "ADMIN";
+                //Hidden html that runs on admin page
+                //navbar with all links
+                echo '
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">EC Labs</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                    <!-- register link >-->
+                        <li class="nav-item active">
+                            <a class="nav-link" href="Login/register.php">Register </a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="Login/logout.php">Log Out </a>
+                        </li>
+                        <li class="nav-item active">
+                        <a class="nav-link" href="View/cart.php">Cart </a>
+                        </li> 
+                        <li class="nav-item active">
+                        <a class="nav-link" href="Admin/Brand.php">Brand </a>
+                        </li>
+                        <li class="nav-item active">
+                        <a class="nav-link" href="Admin/category.php">Category </a>
+                        </li>
+                        <!->
+        
+                    </ul>
+                </div>
+            </nav>
+                ';
+                //html that runs for regular user
+                //navbar with reduced links
+            } else {
+                echo "USER";
+                echo '
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">EC Labs</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <!-- register link >-->
+                        <li class="nav-item active">
+                            <a class="nav-link" href="Login/register.php">Register </a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="Login/logout.php">Log Out </a>
+                        </li>
+                        <!-- <li class="nav-item active">
+                        <a class="nav-link" href="View/cart.php">Cart </a>
+                    </li> -->
+                        <!->
+        
+                    </ul>
+                </div>
+            </nav>
+                ';
+            }
+        }
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">EC Labs</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link" href="Login/register.php">Register </a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="Login/login.php">LogIn </a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="View/cart.php">Cart </a>
-            </li>
-            <?php
-            // if (SESSION) {
-            //     # code...
-            // }
-            ?>
-            
+        //code below runs if user is not logged in
+        //redirect to login page
+        else {
+            header("location: Login/login.php");
+        }
+        ?> -
 
-            <?php
-                require 'Settings/core.php';
 
-                // put this in a function in core.php (login_menu())
-                login_menu('Login/login.php', '#');
-                brand_menu_item('Admin/Brand.php');
-            ?>
-              
-            </ul>
-        </div>
-    	</nav>
+    <!-- bootstrap -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 
-        <!-- bootstrap -->
-        <script 
-            src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-            integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-            crossorigin="anonymous"
-        ></script>
-    <?php
-
-    // session_start();
-    // if (isset($_SESSION["loggedin"]) &&$_SESSION["loggedin"]===true) {
-    //     echo '<a href="Login\logout.php">Logout</a>';
-    // }
-    // else {
-    //     if (isset($_SESSION["loggedin"]) &&$_SESSION["loggedin"]===true) {
-    //         echo '<a href="Login\login.php">Login</a>';
-    //     }
-    
-    // }
-    ?>
 </body>
+
 </html>
